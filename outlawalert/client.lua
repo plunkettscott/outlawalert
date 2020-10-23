af04e101a657facd0b186ffa3cc551fe427b6f2f
Scott Plunkett <scott@contaim.io>
2020-10-23 05:00:43 +0000

    dont alert for those with alerts active

--- outlawalert/client.lua
+++ outlawalert/client.lua
@@ -58,7 +58,7 @@ end)
 RegisterNetEvent('thiefPlace')
 AddEventHandler('thiefPlace', function(tx, ty, tz)
     for i = 0, #PedModels do
-        if not origin and IsPedModel(GetPlayerPed(-1),GetHashKey(PedModels[i])) then
+        if not origin and isActive then
             if carJackingAlert then
                 local transT = 250
                 local thiefBlip = AddBlipForCoord(tx, ty, tz)
@@ -83,7 +83,7 @@ end)
 RegisterNetEvent('gunshotPlace')
 AddEventHandler('gunshotPlace', function(gx, gy, gz)
     for i = 0, #PedModels do
-        if not origin and IsPedModel(GetPlayerPed(-1),GetHashKey(PedModels[i])) then
+        if not origin and isActive then
             if gunshotAlert then
                 local transG = 250
                 local gunshotBlip = AddBlipForCoord(gx, gy, gz)
@@ -108,7 +108,7 @@ end)
 RegisterNetEvent('meleePlace')
 AddEventHandler('meleePlace', function(mx, my, mz)
    for i = 0, #PedModels do
-        if not origin and IsPedModel(GetPlayerPed(-1),GetHashKey(PedModels[i])) then
+        if not origin and isActive then
             if meleeAlert then
                 local transM = 250
                 local meleeBlip = AddBlipForCoord(mx, my, mz)
@@ -175,87 +175,93 @@ end)
 
 Citizen.CreateThread( function()
     while true do
-        Wait(0)
-        local plyPos = GetEntityCoords(GetPlayerPed(-1),  true)
-        local s1, s2 = Citizen.InvokeNative( 0x2EB41072B4C1E4C0, plyPos.x, plyPos.y, plyPos.z, Citizen.PointerValueInt(), Citizen.PointerValueInt() )
-        local street1 = GetStreetNameFromHashKey(s1)
-        local street2 = GetStreetNameFromHashKey(s2)
-        if IsPedTryingToEnterALockedVehicle(GetPlayerPed(-1)) or IsPedJacking(GetPlayerPed(-1)) then
-            origin = true
-            DecorSetInt(GetPlayerPed(-1), "IsOutlaw", 2)
-            local male = IsPedMale(GetPlayerPed(-1))
-            if male then
-                sex = "men"
-            elseif not male then
-                sex = "women"
+        if not isActive then
+            Wait(0)
+            local plyPos = GetEntityCoords(GetPlayerPed(-1),  true)
+            local s1, s2 = Citizen.InvokeNative( 0x2EB41072B4C1E4C0, plyPos.x, plyPos.y, plyPos.z, Citizen.PointerValueInt(), Citizen.PointerValueInt() )
+            local street1 = GetStreetNameFromHashKey(s1)
+            local street2 = GetStreetNameFromHashKey(s2)
+            if IsPedTryingToEnterALockedVehicle(GetPlayerPed(-1)) or IsPedJacking(GetPlayerPed(-1)) then
+                origin = true
+                DecorSetInt(GetPlayerPed(-1), "IsOutlaw", 2)
+                local male = IsPedMale(GetPlayerPed(-1))
+                if male then
+                    sex = "men"
+                elseif not male then
+                    sex = "women"
+                end
+                TriggerServerEvent('thiefInProgressPos', plyPos.x, plyPos.y, plyPos.z)
+                local veh = GetVehiclePedIsTryingToEnter(GetPlayerPed(-1))
+                local vehName = GetDisplayNameFromVehicleModel(GetEntityModel(veh))
+                local vehName2 = GetLabelText(vehName)
+                if s2 == 0 then
+                    TriggerServerEvent('thiefInProgressS1', street1, vehName2, sex)
+                elseif s2 ~= 0 then
+                    TriggerServerEvent('thiefInProgress', street1, street2, vehName2, sex)
+                end
+                Wait(5000)
+                origin = false
             end
-            TriggerServerEvent('thiefInProgressPos', plyPos.x, plyPos.y, plyPos.z)
-            local veh = GetVehiclePedIsTryingToEnter(GetPlayerPed(-1))
-            local vehName = GetDisplayNameFromVehicleModel(GetEntityModel(veh))
-            local vehName2 = GetLabelText(vehName)
-            if s2 == 0 then
-                TriggerServerEvent('thiefInProgressS1', street1, vehName2, sex)
-            elseif s2 ~= 0 then
-                TriggerServerEvent('thiefInProgress', street1, street2, vehName2, sex)
-            end
-            Wait(5000)
-            origin = false
         end
     end
 end)
 
 Citizen.CreateThread( function()
     while true do
-        Wait(0)
-        local plyPos = GetEntityCoords(GetPlayerPed(-1),  true)
-        local s1, s2 = Citizen.InvokeNative( 0x2EB41072B4C1E4C0, plyPos.x, plyPos.y, plyPos.z, Citizen.PointerValueInt(), Citizen.PointerValueInt() )
-        local street1 = GetStreetNameFromHashKey(s1)
-        local street2 = GetStreetNameFromHashKey(s2)
-        if IsPedInMeleeCombat(GetPlayerPed(-1)) then 
-            origin = true
-            DecorSetInt(GetPlayerPed(-1), "IsOutlaw", 2)
-            local male = IsPedMale(GetPlayerPed(-1))
-            if male then
-                sex = "men"
-            elseif not male then
-                sex = "women"
+        if not isActive then
+            Wait(0)
+            local plyPos = GetEntityCoords(GetPlayerPed(-1),  true)
+            local s1, s2 = Citizen.InvokeNative( 0x2EB41072B4C1E4C0, plyPos.x, plyPos.y, plyPos.z, Citizen.PointerValueInt(), Citizen.PointerValueInt() )
+            local street1 = GetStreetNameFromHashKey(s1)
+            local street2 = GetStreetNameFromHashKey(s2)
+            if IsPedInMeleeCombat(GetPlayerPed(-1)) then 
+                origin = true
+                DecorSetInt(GetPlayerPed(-1), "IsOutlaw", 2)
+                local male = IsPedMale(GetPlayerPed(-1))
+                if male then
+                    sex = "men"
+                elseif not male then
+                    sex = "women"
+                end
+                TriggerServerEvent('meleeInProgressPos', plyPos.x, plyPos.y, plyPos.z)
+                if s2 == 0 then
+                    TriggerServerEvent('meleeInProgressS1', street1, sex)
+                elseif s2 ~= 0 then
+                    TriggerServerEvent("meleeInProgress", street1, street2, sex)
+                end
+                Wait(3000)
+                origin = false
             end
-            TriggerServerEvent('meleeInProgressPos', plyPos.x, plyPos.y, plyPos.z)
-            if s2 == 0 then
-                TriggerServerEvent('meleeInProgressS1', street1, sex)
-            elseif s2 ~= 0 then
-                TriggerServerEvent("meleeInProgress", street1, street2, sex)
-            end
-            Wait(3000)
-            origin = false
         end
     end
 end)
 
 Citizen.CreateThread( function()
     while true do
-        Wait(0)
-        local plyPos = GetEntityCoords(GetPlayerPed(-1),  true)
-        local s1, s2 = Citizen.InvokeNative( 0x2EB41072B4C1E4C0, plyPos.x, plyPos.y, plyPos.z, Citizen.PointerValueInt(), Citizen.PointerValueInt() )
-        local street1 = GetStreetNameFromHashKey(s1)
-        local street2 = GetStreetNameFromHashKey(s2)
-        if IsPedShooting(GetPlayerPed(-1)) then
-            origin = true
-            DecorSetInt(GetPlayerPed(-1), "IsOutlaw", 2)
-            local male = IsPedMale(GetPlayerPed(-1))
-            if male then
-                sex = "men"
-            elseif not male then
-                sex = "women"
+        if not isActive then
+            Wait(0)
+            local plyPos = GetEntityCoords(GetPlayerPed(-1),  true)
+            local s1, s2 = Citizen.InvokeNative( 0x2EB41072B4C1E4C0, plyPos.x, plyPos.y, plyPos.z, Citizen.PointerValueInt(), Citizen.PointerValueInt() )
+            local street1 = GetStreetNameFromHashKey(s1)
+            local street2 = GetStreetNameFromHashKey(s2)
+            if IsPedShooting(GetPlayerPed(-1)) then
+                origin = true
+                DecorSetInt(GetPlayerPed(-1), "IsOutlaw", 2)
+                local male = IsPedMale(GetPlayerPed(-1))
+                if male then
+                    sex = "men"
+                elseif not male then
+                    sex = "women"
+                end
+                TriggerServerEvent('gunshotInProgressPos', plyPos.x, plyPos.y, plyPos.z)
+                if s2 == 0 then
+                    TriggerServerEvent('gunshotInProgressS1', street1, sex)
+                elseif s2 ~= 0 then
+                    TriggerServerEvent("gunshotInProgress", street1, street2, sex)
+                end
+                Wait(3000)
+                origin = false
             end
-            TriggerServerEvent('gunshotInProgressPos', plyPos.x, plyPos.y, plyPos.z)
-            if s2 == 0 then
-                TriggerServerEvent('gunshotInProgressS1', street1, sex)
-            elseif s2 ~= 0 then
-                TriggerServerEvent("gunshotInProgress", street1, street2, sex)
-            end
-            Wait(3000)
-            origin = false
         end
     end
 end)
